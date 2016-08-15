e4a08aaf102a0effed23604e0e65cefefb2523ad
Razlagutt <razlagutt@yandex.ru>
2016-08-15 19:59:21 +0300

    Tasks Of The Chapter 1 is done. version 01

---
+++ java-a-to-z/chapter_01/lesson_05/src/main/java/ru/ablokhin/l_05/ArrayRotationMain.java
@@ -0,0 +1,7 @@
+package ru.ablokhin.l_05;
+
+/**
+ * Created by Andrey on 15.08.2016.
+ */
+public class arrayRotationMain {
+}
---
+++ java-a-to-z/chapter_01/lesson_06/src/test/java/ru/ablokhin/l_06/SubstringSearcherTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andrey on 15.08.2016.
+ */
+public class SubstringSearcherTest {
+
+}
