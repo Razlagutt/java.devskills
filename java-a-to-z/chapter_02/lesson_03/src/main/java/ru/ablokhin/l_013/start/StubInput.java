7bf2247f129e2dadba9725e4e0a9100e735beb92
Razlagutt <razlagutt@yandex.ru>
2016-08-24 22:09:40 +0300

    Task 03.2 Of Chapter 02 Is Done

---
+++ java-a-to-z/chapter_02/lesson_03/src/main/java/ru/ablokhin/l_013/start/StubInput.java
@@ -0,0 +1,15 @@
+package ru.ablokhin.l_013.start;
+
+public class StubInput implements Input{
+	
+	private String[] answers;
+	private int indx = 0;
+	
+	public StubInput(String[] answers){
+		this.answers = answers;
+	}
+	
+	public String ask(String question){
+		return answers[indx++];
+	}
+}
