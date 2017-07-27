6ff1ce0aaf1a16a0e274b4c8527a2448b33ee40e
Andrey <razlagutt@mail.ru>
2017-07-27 14:20:08 +0300

    Chapter_03 lesson_301 is done

--- java-a-to-z/chapter_03/lesson_301/src/main/java/ru/ablokhin/l301/MyCollection.java
+++ java-a-to-z/chapter_03/lesson_301/src/main/java/ru/ablokhin/l301/MyCollection.java
@@ -3,24 +3,48 @@ package ru.ablokhin.l301;
 import java.util.Collection;
 
 /**
- * Created by razla on 06.05.2017.
+ * Класс, замеряющий время вставки и удаления большого количества случайных строк.
+ * @author ablokhin
+ * @since 06.05.2017
+ * @version 1
  */
 public class MyCollection {
 
-    public long add(Collection<String> collection, String line, int amount) {
+    /**
+     * Случайная строка
+     */
+    String line;
+
+    public MyCollection(String line){
+        this.line = line;
+    }
+
+    /**
+     * Метод добавления большого количества случайных строк.
+     * @param collection коллекция, в которую нужно вставить строки
+     * @param amount число строк для вставки в коллекцию
+     * @return время, затраченное на выполнение
+     */
+    public long add(Collection<String> collection, int amount) {
         long startTime = System.nanoTime();
         for (int i = 0; i < amount; i++) {
-            collection.add(line + String.valueOf(i));
+            collection.add(this.line + String.valueOf(i));
         }
         long endTime = System.nanoTime();
         long difference = endTime - startTime;
         return difference;
     }
 
-    public long delete(Collection<String> collection, String line, int amount) {
+    /**
+     * Метод удаления большого количества случайных строк.
+     * @param collection коллекция, из которой нужно удалить строки
+     * @param amount число первых строк для удаления из коллекции
+     * @return время, затраченное на выполнение
+     */
+    public long delete(Collection<String> collection, int amount) {
         long startTime = System.nanoTime();
         for (int i = 0; i < amount; i++) {
-            collection.remove(line + String.valueOf(i));
+            collection.remove(this.line + String.valueOf(i));
         }
         long endTime = System.nanoTime();
         long difference = endTime - startTime;
--- java-a-to-z/chapter_03/lesson_301/src/test/java/ru/ablokhin/l301/MyCollectionTest.java
+++ java-a-to-z/chapter_03/lesson_301/src/test/java/ru/ablokhin/l301/MyCollectionTest.java
@@ -8,23 +8,44 @@ import java.util.LinkedList;
 import java.util.TreeSet;
 
 /**
- * Created by razla on 06.05.2017.
+ * Класс замеряет время вставки и удаления большого количества случайных строк.
+ * @author ablokhin
+ * @since 06.05.2017
+ * @version 1
  */
 public class MyCollectionTest {
+
     Collection<String> arrList = new ArrayList<>();
     Collection<String> linList = new LinkedList<>();
     Collection<String> tSet = new TreeSet<>();
-    String line = "Что такое Lorem Ipsum";
     int amountToAdd = 99999;//354836040;
     int amountToDel = 99999;
-    MyCollection myCollection = new MyCollection();
+    MyCollection myCollection = new MyCollection("Что такое Lorem Ipsum");
 
+    /**
+     * Метод добавления большого количества случайных строк.
+     * @param myCollection объект, содержащий случайную строку
+     * @param amountToAdd число строк для вставки в коллекцию
+     * @param collection коллекция, в которую нужно вставить строки
+     * @return коллекцию случайных строк
+     */
+    public Collection<String> addToCollection(MyCollection myCollection, int amountToAdd, Collection<String> collection){
+        for (int i = 0; i < amountToAdd; i++) {
+            collection.add(myCollection.line + String.valueOf(i));
+        }
+        return collection;
+    }
+
+    /**
+     * Метод замеряет время вставки и удаления большого количества случайных строк.
+     * @throws Exception
+     */
     @Test
     public void performanceTest() throws Exception {
         String strFormatAmount = String.format("Время вставки в коллекцию %s случайных строк составило:", amountToAdd);
-        String stringFormatArrayList = String.format("ArrayList = %d мс.", myCollection.add(arrList, line, amountToAdd));
-        String stringFormatLinkedList = String.format("LinkedList = %d мс.", myCollection.add(linList, line, amountToAdd));
-        String stringFormatTreeSet = String.format("TreeSet = %d мс.", myCollection.add(tSet, line, amountToAdd));
+        String stringFormatArrayList = String.format("ArrayList = %d мс.", myCollection.add(arrList, amountToAdd));
+        String stringFormatLinkedList = String.format("LinkedList = %d мс.", myCollection.add(linList, amountToAdd));
+        String stringFormatTreeSet = String.format("TreeSet = %d мс.", myCollection.add(tSet, amountToAdd));
 
         System.out.println(strFormatAmount);
         System.out.println(stringFormatArrayList);
@@ -33,9 +54,12 @@ public class MyCollectionTest {
         System.out.println("------------------------");
 
         strFormatAmount = String.format("Время удаления из коллекции %s случайных строк составило:", amountToDel);
-        stringFormatArrayList = String.format("ArrayList = %d мс.", myCollection.delete(arrList, line, amountToDel));
-        stringFormatLinkedList = String.format("LinkedList = %d мс.", myCollection.delete(linList, line, amountToDel));
-        stringFormatTreeSet = String.format("TreeSet = %d мс.", myCollection.delete(tSet, line, amountToDel));
+        stringFormatArrayList = String.format("ArrayList = %d мс.",
+                myCollection.delete(this.addToCollection(myCollection, amountToAdd, arrList), amountToDel));
+        stringFormatLinkedList = String.format("LinkedList = %d мс.",
+                myCollection.delete(this.addToCollection(myCollection, amountToAdd, linList), amountToDel));
+        stringFormatTreeSet = String.format("TreeSet = %d мс.",
+                myCollection.delete(this.addToCollection(myCollection, amountToAdd, tSet), amountToDel));
 
         System.out.println(strFormatAmount);
         System.out.println(stringFormatArrayList);
